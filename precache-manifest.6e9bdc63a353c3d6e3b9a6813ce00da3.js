b0b6fd6b0b5196b9f1745a3b93222b60fd7b2b00
Awu Chen <awwu@Mac.lan>
2026-09-18 12:08:30 -0400

    Updates

---
+++ precache-manifest.6e9bdc63a353c3d6e3b9a6813ce00da3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac66d16a0657d4c8ae7d10d83f2f2f7",
+    "url": "/ResearchNet/index.html"
+  },
+  {
+    "revision": "40348612da2fddecb10c",
+    "url": "/ResearchNet/static/css/main.ead59979.chunk.css"
+  },
+  {
+    "revision": "555f1ef945b64c879189",
+    "url": "/ResearchNet/static/js/2.92d3b88c.chunk.js"
+  },
+  {
+    "revision": "7b36c4a4b00d4c1eb1f21a15ccb34795",
+    "url": "/ResearchNet/static/js/2.92d3b88c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40348612da2fddecb10c",
+    "url": "/ResearchNet/static/js/main.782985e4.chunk.js"
+  },
+  {
+    "revision": "b7f1a66234c5a765179e",
+    "url": "/ResearchNet/static/js/runtime-main.d0c56513.js"
+  }
+]);
